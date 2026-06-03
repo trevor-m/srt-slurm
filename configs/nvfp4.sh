45b86abd4b538b96d958939803a21a1d1e9f1a93
Trevor Morris <tmorris@nvidia.com>
2026-06-02 18:18:01 -0700

    add setup script

---
+++ configs/nvfp4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+BRANCH="dsv4-fp4"
+
+cd /sgl-workspace/sglang
+git remote remove origin
+git remote add origin https://github.com/trevor-m/sglang.git
+git fetch origin
+git checkout origin/${BRANCH}
