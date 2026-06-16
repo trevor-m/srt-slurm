e8040076088001029d70013947a84777ef36acc5
Trevor Morris <tmorris@nvidia.com>
2026-06-16 11:24:47 -0700

    Add aime25 configs

--- configs/nvfp4.sh
+++ configs/nvfp4.sh
@@ -8,3 +8,5 @@ git remote remove origin
 git remote add origin https://github.com/trevor-m/sglang.git
 git fetch origin
 git checkout origin/${BRANCH}
+
+git cherry-pick 4efe6675af3a17846cfa8296e8476526372eaefc
